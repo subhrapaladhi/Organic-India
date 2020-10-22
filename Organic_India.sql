61a931ee51ed745e1215824c49de177856590dc0
subhra paladhi <subhrapaladhi9@gmail.com>
2020-10-22 08:28:44 +0530

    exorting latest db

--- Organic_India.sql
+++ Organic_India.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 20, 2020 at 07:55 AM
+-- Generation Time: Oct 22, 2020 at 04:57 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -31,18 +31,22 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `product_name` varchar(25) NOT NULL,
   `product_price` float DEFAULT NULL,
-  `product_image` varchar(100) DEFAULT NULL
+  `product_image` varchar(100) DEFAULT NULL,
+  `product_description` text NOT NULL,
+  `sellerid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_image`) VALUES
-(1, 'Fertilizer Pack', 1799, './upload/product1.png'),
-(2, 'Lawn Mower', 147, './upload/product2.png'),
-(3, 'Jowar Seeds (500 kg)', 459, './upload/product3.png'),
-(4, 'Pesticide Pack', 278, './upload/product4.png');
+INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_image`, `product_description`, `sellerid`) VALUES
+(1, 'Fertilizer Pack', 1799, 'product1.png', '', 1),
+(2, 'Lawn Mower', 147, 'product2.png', '', 2),
+(3, 'Jowar Seeds (500 kg)', 459, 'product3.png', '', 1),
+(4, 'Pesticide Pack', 278, 'product4.png', '', 0),
+(5, 'tempimagetest', 1234, '1272296725.png', '', 1),
+(6, 'eiourewio', 9343, '681061182.png', 'kldskl;fkls d;lfjsdk\r\ndflkskjd', 1);
 
 -- --------------------------------------------------------
 
@@ -65,7 +69,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `address`, `mobile`) VALUES
 (1, 'Subhra Paladhi', 'subhrapaladhi9@gmail.com', 'dd130a849d7b29e5541b05d2f7f86a4acd4f1ec598c1c9438783f56bc4f0ff80', 'Room no. 376', '9003814273'),
-(2, 'kumar', 'kumar@gmail.com', 'dd130a849d7b29e5541b05d2f7f86a4acd4f1ec598c1c9438783f56bc4f0ff80', 'Street 233 ,kljfsa, !# ffkalsk', '9003814273');
+(2, 'kumar', 'kumar@gmail.com', 'dd130a849d7b29e5541b05d2f7f86a4acd4f1ec598c1c9438783f56bc4f0ff80', 'Street 233 ,kljfsa, !# ffkalsk', '9003814273'),
+(3, 'ramesh', 'ramesh@gmail.com', 'dd130a849d7b29e5541b05d2f7f86a4acd4f1ec598c1c9438783f56bc4f0ff80', 'dflkajs 32423 ffdlksjfsl', '9003814273');
 
 --
 -- Indexes for dumped tables
@@ -91,13 +96,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
